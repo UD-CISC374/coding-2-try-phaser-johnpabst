02cd829440eb6346ba1dd9866b3bf6f41ec435a4
johnpabst <60942062+johnpabst@users.noreply.github.com>
2020-02-26 02:50:18 -0500

    added animations

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -20,9 +20,64 @@ export default class MainScene extends Phaser.Scene {
     this.ship3 = this.add.sprite(this.scale.width / 2 + 50, this.scale.height / 2, "ship3");
 
     this.add.text(20,20, "This is the Game");
-    this.exampleObject = new ExampleObject(this, 0, 0);
+    this.anims.create({
+      key:"ship1_anim",
+      frames: this.anims.generateFrameNumbers("ship",{ start: 0, end: 1 }),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key:"ship2_anim",
+      frames: this.anims.generateFrameNumbers("ship2",{ start: 0, end: 1 }),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key:"ship3_anim",
+      frames: this.anims.generateFrameNumbers("ship3",{ start: 0, end: 1 }),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key:"explode",
+      frames: this.anims.generateFrameNumbers("explosion",{ start: 0, end: 1 }),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true
+    });
+
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
+
+    this.ship1.setInteractive();
+    this.ship2.setInteractive();
+    this.ship3.setInteractive();
+
+    this.input.on('gameobjectdown',this.destroyShip, this);
   }
 
+  moveShip(ship, speed){
+    ship.y += speed;
+    if (ship.y > this.scale.height){
+      this.resetShipPos(ship);
+    }
+  }
   update() {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip(this.ship3, 3);
+
+    this.background.tilePositionY -= 0.5;
+  }
+  resetShipPos(ship){
+    ship.y=0;
+    var randomX = Phaser.Math.Between(0, this.scale.width);
+    ship.x = randomX;
+  }
+
+  destroyShip(pointer, gameObject){
+    gameObject.setTexture("explosion");
+    gameObject.play("explode");
   }
 }
