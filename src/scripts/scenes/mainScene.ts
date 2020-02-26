d037935826e9b6ae2b3e5515a7eebd27bcf0f77b
johnpabst <60942062+johnpabst@users.noreply.github.com>
2020-02-26 01:25:34 -0500

    visual bug fix attempt1

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,22 +2,22 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
-  background: Phaser.GameObjects.Image;
-  ship1: Phaser.GameObjects.Image;
-  ship2: Phaser.GameObjects.Image;
-  ship3: Phaser.GameObjects.Image;
+  background: Phaser.GameObjects.TileSprite;
+  ship1: Phaser.GameObjects.Sprite;
+  ship2: Phaser.GameObjects.Sprite;
+  ship3: Phaser.GameObjects.Sprite;
 
   constructor() {
     super({ key: 'MainScene' });
   }
 
   create() {
-    this.background = this.add.image(0,0,"background");
+    this.background = this.add.tileSprite(0,0,this.scale.width, this.scale.height,"background");
     this.background.setOrigin(0,0);
 
-    this.ship1 = this.add.image(this.scale.width / 2 - 50, this.scale.height / 2, "ship");
-    this.ship2 = this.add.image(this.scale.width / 2, this.scale.height / 2, "ship2");
-    this.ship3 = this.add.image(this.scale.width / 2 + 50, this.scale.height / 2, "ship3");
+    this.ship1 = this.add.sprite(this.scale.width / 2 - 50, this.scale.height / 2, "ship");
+    this.ship2 = this.add.sprite(this.scale.width / 2, this.scale.height / 2, "ship2");
+    this.ship3 = this.add.sprite(this.scale.width / 2 + 50, this.scale.height / 2, "ship3");
 
     this.add.text(20,20, "This is the Game");
     this.exampleObject = new ExampleObject(this, 0, 0);
