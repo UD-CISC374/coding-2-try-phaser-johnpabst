16ea5c9b1632714dbae13b3f74b94d6b3a644624
johnpabst <60942062+johnpabst@users.noreply.github.com>
2020-02-26 00:59:42 -0500

    Updates

---
+++ dev/precache-manifest.6a5e9dda82431331fe53c5cf9567334b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/educational-phaser-game-template/devassets/background.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-phaser-game-template/devassets/citations.md"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/educational-phaser-game-template/devassets/colors.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/educational-phaser-game-template/devassets/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/educational-phaser-game-template/devassets/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/educational-phaser-game-template/devassets/ship3.png"
+  },
+  {
+    "revision": "4eeddb9fad78f3042659",
+    "url": "/educational-phaser-game-template/devgen.main.854afe072bb09f0834b1.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/educational-phaser-game-template/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "1d37d9ea3cd5c6efd2a46259d7beba6a",
+    "url": "/educational-phaser-game-template/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/educational-phaser-game-template/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-phaser-game-template/devsw.js"
+  }
+]);
