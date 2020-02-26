c9a4b616a7c042ee65bf90bcfd73d4b51c952be6
johnpabst <60942062+johnpabst@users.noreply.github.com>
2020-02-26 01:52:49 -0500

    Updates

---
+++ dev/precache-manifest.e183b4a8c53a8d89ca191f8615a44b56.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-johnpabst/devassets/background.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-johnpabst/devassets/citations.md"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-johnpabst/devassets/colors.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-johnpabst/devassets/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-johnpabst/devassets/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-johnpabst/devassets/ship3.png"
+  },
+  {
+    "revision": "ea8243f9dc9343b26265",
+    "url": "/coding-2-try-phaser-johnpabst/devgen.main.013594ecb355033bbfd2.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-johnpabst/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "e7f86f84f15eb175d0dd31b03e444dcf",
+    "url": "/coding-2-try-phaser-johnpabst/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-johnpabst/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-johnpabst/devsw.js"
+  }
+]);
--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-phaser-game-template/devprecache-manifest.3a1ba99c3b301588066bccf7b8332ecf.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-johnpabst/devprecache-manifest.e183b4a8c53a8d89ca191f8615a44b56.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
