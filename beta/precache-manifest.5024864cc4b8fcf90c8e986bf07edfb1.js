ae9507707beeef182ad90298eae218af0e0fc963
johnpabst <60942062+johnpabst@users.noreply.github.com>
2020-02-27 21:45:04 -0500

    Updates

---
+++ beta/precache-manifest.5024864cc4b8fcf90c8e986bf07edfb1.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/background.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/citations.md"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/colors.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/font/font.xml"
+  },
+  {
+    "revision": "247595700fd953e877b582a3d8ce58e6",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/psychedelic_bg.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/ship3.png"
+  },
+  {
+    "revision": "86b88d1e4615b77a8b94a93ba82500bf",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/sounds/beam.mp3"
+  },
+  {
+    "revision": "ce33e3b835ac69e0766dbe6d7c67dfc1",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/sounds/beam.ogg"
+  },
+  {
+    "revision": "f8083b2c5cc44105a173fade6f9640a2",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/sounds/earthbound_saturn_ness.mp3"
+  },
+  {
+    "revision": "ce8dbd56bcb81632df7279e91fdcec54",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/sounds/explosion.mp3"
+  },
+  {
+    "revision": "4b9be0c2fdf194385275c2e8d006949b",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/sounds/explosion.ogg"
+  },
+  {
+    "revision": "2101f87289e14406eb4b215cf55a3ebc",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/sounds/pickup.mp3"
+  },
+  {
+    "revision": "2caf90e1d1576c0a7891bfb9723ded61",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/sounds/pickup.ogg"
+  },
+  {
+    "revision": "da087af3292fb062136a96eb496ceaf7",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/sounds/sci-fi_platformer12.mp3"
+  },
+  {
+    "revision": "240502f3ab4e1955dd49c1dbf827e247",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/sounds/sci-fi_platformer12.ogg"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/spritesheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-johnpabst/betaassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "be6ce8cf580af0e41874",
+    "url": "/coding-2-try-phaser-johnpabst/betagen.main.5c259b3effef0319c257.bundle.js"
+  },
+  {
+    "revision": "7e3abc0967d71642756e",
+    "url": "/coding-2-try-phaser-johnpabst/betagen.vendors.bbe9474c65e057812275.bundle.js"
+  },
+  {
+    "revision": "8747c47cdf3d4ead2918f204dc5ced6e",
+    "url": "/coding-2-try-phaser-johnpabst/betaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-johnpabst/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-johnpabst/betasw.js"
+  }
+]);
