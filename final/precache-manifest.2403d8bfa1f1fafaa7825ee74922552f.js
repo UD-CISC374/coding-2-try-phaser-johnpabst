5366a3a125f90cac1ac397b770f8b321e445f230
johnpabst <60942062+johnpabst@users.noreply.github.com>
2020-02-28 14:34:47 -0500

    Updates

---
+++ final/precache-manifest.2403d8bfa1f1fafaa7825ee74922552f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/background.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/citations.md"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/colors.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/font/font.xml"
+  },
+  {
+    "revision": "247595700fd953e877b582a3d8ce58e6",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/psychedelic_bg.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/ship3.png"
+  },
+  {
+    "revision": "86b88d1e4615b77a8b94a93ba82500bf",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/sounds/beam.mp3"
+  },
+  {
+    "revision": "ce33e3b835ac69e0766dbe6d7c67dfc1",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/sounds/beam.ogg"
+  },
+  {
+    "revision": "f8083b2c5cc44105a173fade6f9640a2",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/sounds/earthbound_saturn_ness.mp3"
+  },
+  {
+    "revision": "ce8dbd56bcb81632df7279e91fdcec54",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/sounds/explosion.mp3"
+  },
+  {
+    "revision": "4b9be0c2fdf194385275c2e8d006949b",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/sounds/explosion.ogg"
+  },
+  {
+    "revision": "2101f87289e14406eb4b215cf55a3ebc",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/sounds/pickup.mp3"
+  },
+  {
+    "revision": "2caf90e1d1576c0a7891bfb9723ded61",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/sounds/pickup.ogg"
+  },
+  {
+    "revision": "da087af3292fb062136a96eb496ceaf7",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/sounds/sci-fi_platformer12.mp3"
+  },
+  {
+    "revision": "240502f3ab4e1955dd49c1dbf827e247",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/sounds/sci-fi_platformer12.ogg"
+  },
+  {
+    "revision": "433c9ca81b44d0315edc233a088deb8b",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "47cf527664eb5291459d9432eb43636d",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/frog.png"
+  },
+  {
+    "revision": "8a1b85c5139be299343504a1538343bf",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/minishCap.png"
+  },
+  {
+    "revision": "e107b6b9c3d3148b3fcb12e2fc9ff5fb",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/nessBike.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "cd409e208843fd986296186bd9deb5cd",
+    "url": "/coding-2-try-phaser-johnpabst/finalassets/spritesheets/totodile.png"
+  },
+  {
+    "revision": "90c65ad636b2f6b158ca",
+    "url": "/coding-2-try-phaser-johnpabst/finalgen.main.5894b2189be8de38ef48.bundle.js"
+  },
+  {
+    "revision": "7e3abc0967d71642756e",
+    "url": "/coding-2-try-phaser-johnpabst/finalgen.vendors.bbe9474c65e057812275.bundle.js"
+  },
+  {
+    "revision": "e1d7e9c1b1913d37f22774eb6f6e15f2",
+    "url": "/coding-2-try-phaser-johnpabst/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-johnpabst/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-johnpabst/finalsw.js"
+  }
+]);
---
+++ final/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-johnpabst/finalprecache-manifest.2403d8bfa1f1fafaa7825ee74922552f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
